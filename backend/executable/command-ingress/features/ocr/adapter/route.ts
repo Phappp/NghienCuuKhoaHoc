a13979e3bc4b5bb63c66e823416154d5aee2c29e
Phappp <161460701+Phappp@users.noreply.github.com>
2025-06-30 09:15:03 +0700

    chore: chỉnh sửa chut xíu route ocr

--- backend/executable/command-ingress/features/ocr/adapter/route.ts
+++ backend/executable/command-ingress/features/ocr/adapter/route.ts
@@ -4,10 +4,6 @@ import { OcrController } from './controller';
 
 export default function initOcrRoute(controller: OcrController): Router {
     const router = Router();
-    router.get('/ping', (req, res) => {
-        res.send('pong');
-    });
-
     router.post('/process', controller.processImage.bind(controller));
     return router;
 }
